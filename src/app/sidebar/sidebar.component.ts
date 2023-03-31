304c59b7a7f9dd6d38c3a2c01e16d0c8bf897f57
Ahlem2009 <111354211+Ahlem2009@users.noreply.github.com>
2023-03-31 15:15:44 +0200

    sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,6 +1,5 @@
 
 import { Component, OnInit } from '@angular/core';
-import { Renderer2, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-sidebar',
@@ -9,23 +8,13 @@ import { Router } from '@angular/router';
 })
 export class SidebarComponent  {
 
-  // isGestionTicketActive: boolean=false;
-  // constructor(private router: Router) {
-  //   this.isGestionTicketActive = this.router.url === '/gestion-ticket';
-  // }
-
-  urli:string[]=['/dashboard', '/gestion-doc', '/creation-Document']
-  constructor(private renderer: Renderer2, private router: Router, private elRef: ElementRef) {}
-
-  // Fonction pour mettre à jour la classe CSS
-  updateClass() {
-    const element = this.elRef.nativeElement.querySelector('#myDiv');
-    for (let i = 1; i <=3; i++) {
-      if (this.router.url === urli[i]) {
-        this.renderer.addClass(element, 'nav-link py-3 px-2'); // Remplacez 'ma-classe-css' par la classe que vous voulez ajouter
-      } else {
-        this.renderer.removeClass(element, 'd-block p-3 link-dark text-decoration-none nav-link py-3 px-2'); // Remplacez 'ma-classe-css' par la classe que vous voulez supprimer
-      }
-    }
+  isGestionTicketActive: boolean=false;
+  constructor(private router: Router) {
+    this.isGestionTicketActive = this.router.url === '/gestion-ticket';
   }
+
+  
+
+
+ 
 }
